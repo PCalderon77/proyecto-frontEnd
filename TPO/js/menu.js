2d8857a2a573321d62ca26fa92cd902b54a96d7a
PCalderon77 <92556323+PCalderon77@users.noreply.github.com>
2022-05-30 00:42:47 -0300

    Add files via upload

---
+++ TPO/js/menu.js
@@ -0,0 +1,15 @@
+const hamburguer = document.querySelector('.nav__img')
+const menu = document.querySelector('.navbar-list')
+
+
+hamburguer.addEventListener('click', ()=>{
+    menu.classList.toggle("spread")
+})
+
+window.addEventListener('click', e =>{
+    if(menu.classList.contains('spread') 
+        && e.target != menu && e.target != hamburguer){
+        console.log('cerrar')
+        menu.classList.toggle("spread")
+    }
+})
