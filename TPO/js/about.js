5714e57c7fd5630969d53c97f3b8325803bcc282
PCalderon77 <92556323+PCalderon77@users.noreply.github.com>
2022-05-30 01:01:44 -0300

    Delete TPO directory

--- TPO/js/about.js
+++
@@ -1,5 +0,0 @@
-const buttonContactForm = document.getElementById("button-bajar")
-const formSection = document.querySelector("#galeria");
-buttonContactForm.addEventListener('click', () => {
-    formSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
-})
